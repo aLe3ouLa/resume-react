eb139db6ea1f012a26a8d9469e367747884f0996
aLe3ouLa <ale3mp90@gmail.com>
2020-04-15 23:00:43 +0200

    Updates

---
+++ precache-manifest.c0a743a0d73220811ce0b4b6243bf5ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d92032665eb1d4850376c1264d8ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8027261b415fbb5afb9",
+    "url": "/static/css/main.6054893b.chunk.css"
+  },
+  {
+    "revision": "db0eb441fda501d02b4c",
+    "url": "/static/js/2.3636a260.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.3636a260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8027261b415fbb5afb9",
+    "url": "/static/js/main.90d6a6bc.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  },
+  {
+    "revision": "91e737874708a61e773298ace378f9d5",
+    "url": "/static/media/1.91e73787.png"
+  },
+  {
+    "revision": "22f767ee2a9b77561d67065224f3b9c6",
+    "url": "/static/media/2.22f767ee.png"
+  },
+  {
+    "revision": "00cc301486e8c079b0f56e436f3dd645",
+    "url": "/static/media/3.00cc3014.png"
+  },
+  {
+    "revision": "8ac01a547f74b9c15cd59d6997d047b3",
+    "url": "/static/media/4.8ac01a54.png"
+  },
+  {
+    "revision": "5397527e9e3d76296273f2eb85e6699b",
+    "url": "/static/media/cv.5397527e.pdf"
+  }
+]);
--- src/styles/defaults/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Reset } from "./reset";
-export { default as Typography } from "./typography";
