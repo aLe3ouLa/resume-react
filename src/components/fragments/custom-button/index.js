1bafbfa07fb827a3431669076e349e57b95bb250
Alexandra Barka <ale3mp90@gmail.com>
2022-01-28 16:39:58 +0100

    Update index.js

--- src/components/fragments/custom-button/index.js
+++ src/components/fragments/custom-button/index.js
@@ -9,12 +9,19 @@ const CustomButtonCtn = styled.a`
   font-size: 1.4rem;
   font-weight: 900;
   text-transform: uppercase;
-  border-radius: 8px;
-  padding: ${({ theme }) => theme.spacing.xs} ${({ theme }) => theme.spacing.md};
+  border-radius: 5px;
+  padding: ${({ theme }) => theme.spacing.xxs} ${({ theme }) => theme.spacing.sm};
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: ${({ theme }) => theme.spacing.sm};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.secondary};
+    border: 3px wavy ${({ theme }) => theme.color.tertiary};
+    color: ${({ theme }) => theme.color.white};
+    text-decoration: none;
+  }
 `;
 
 const CustomButton = ({ children }) => {
